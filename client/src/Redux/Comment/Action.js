9397ba00bc5e6842da765264cebb531a8d91ffb8
ashokzarmariyaa <ashok.zarmariya@permissionles.org>
2023-03-09 12:00:29 +0530

    creat like comment action

--- client/src/Redux/Comment/Action.js
+++ client/src/Redux/Comment/Action.js
@@ -1,3 +1,5 @@
+import { LIKE_COMMENT } from "./ActionType";
+
 export const createComment = (dispatch) => async (data) => {
   const res = await fetch("http://localhost:5454/api/comments/create", {
     method: "POST",
@@ -26,4 +28,17 @@ export const findPostComment=(dispatch)=>async(data)=>{
     })
     const resData=await res.json();
     dispatch({type:"GET_USER_POST",paylod:resData});
+}
+
+export const likeComment=(dispatch)=>async(data)=>{
+    const res= await fetch("http://localhost:5454/api/comments/commentId",{
+        method:"PUT",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization":'Beare '+data.jwt,
+        },
+        body:JSON.stringify(data.data)
+    })
+    const resData=await res.json();
+    dispatch({type:LIKE_COMMENT,paylod:resData});
 }
